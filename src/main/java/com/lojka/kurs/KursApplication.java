cd2c53ed9470c229b342ff313640baaf890e27ed
liubart123 <Lojka@tut.by>
2020-09-27 12:33:14 +0300

    java commit3

---
+++ src/main/java/com/lojka/kurs/KursApplication.java
@@ -0,0 +1,13 @@
+package com.lojka.kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursApplication.class, args);
+    }
+
+}
